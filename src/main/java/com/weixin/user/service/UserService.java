ef83d57d9e513683994c5272cb44fc448143779d
gaohe <904724283@qq.com>
2016-06-09 17:47:02 +0800

    删减

--- src/main/java/com/weixin/user/service/UserService.java
+++ src/main/java/com/weixin/user/service/UserService.java
@@ -4,7 +4,6 @@ import com.weixin.base.service.BaseService;
 import com.weixin.user.model.User;
 
 public interface UserService  extends BaseService<User>{
-
-	String queryOpenidByCode(String code);
+ 
 
 }
--- src/test/java/com/common/package-info.java
+++ src/test/java/com/common/package-info.java
@@ -1,9 +1,2 @@
-/**
- * @author 高鹤
- * 
- * @2016年6月5日
- *
- * 作用:
- *
- */
+
 package com.common;
